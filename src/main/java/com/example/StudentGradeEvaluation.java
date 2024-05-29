483a950fc8e443c33840776c9247d23f9024eb09
ravikumar-rskd <pilakaravi.rs143@gmail.com>
2024-05-29 08:48:13 +0530

    Ravi output

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -24,13 +24,13 @@ public class StudentGradeEvaluation {
         if(score>=90){
             System.out.println("Grade: A");
         }
-        else if(score>80){
+        else if(score>=80){
             System.out.println("Grade: B");
         }
-        else if(score>70){
+        else if(score>=70){
             System.out.println("Grade: C");
         }
-        else if(score>60)
+        else if(score>=60)
         {
             System.out.println("Grade: D");
         }
